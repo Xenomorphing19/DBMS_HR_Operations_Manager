3cb8adf4b864bda22b6314dc9214f7651c12118b
Xenomorphing19 <anirudharora19@gmail.com>
2021-04-30 19:00:22 +0530

    some work

--- client/applicant-form.js
+++ client/applicant-form.js
@@ -1,5 +1,35 @@
-document.addEventListener('DOMContentLoaded',function(){
+const submitBtn = document.querySelector('#submit-btn');
 
-});
+submitBtn.onclick = function(){
+    const nameInput = document.querySelector('#name');
+    const ageInput = document.querySelector('#age');
+    const addressInput = document.querySelector('#address');
+    const mobileInput = document.querySelector('#mobile');
 
-function
+    const name = nameInput.value;
+    const age = ageInput.value;
+    const address = addressInput.value;
+    const mobile = mobileInput.value;
+    nameInput.value = "";
+    ageInput.value = "";
+    addressInput.value = "";
+    mobileInput.value = "";
+
+    console.log(name);
+
+    fetch('http://localhost:5000/insert', {
+        headers: {
+            'Content-type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify({name: name, age: age, address: address, mobile: mobile})
+    })
+    .then(response => response.json())
+    .then(data => insertRowIntoTable(data['data']));
+
+    
+}
+
+function insertRowIntoTable(data){
+
+}
--- server/app.js
+++ server/app.js
@@ -4,16 +4,37 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
 
+const dbService = require('./dbService');
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended : false}));
 
 //create
 app.post('/insert', (request, response) => {
+    const {name} = request.body.name;
+    const {age} = request.body.age;
+    const {address} = request.body.address;
+    const {mobile} = request.body.mobile;
+    const db = dbService.getDbServiceInstance();
 
+    const result = db.insertNewName(name, age, address, mobile);
+
+    result
+    .then(data => response.json({success: true}))
+    .catch(err => console.log(err));
 });
 
 //read
-app.get('getAll', (request, response) =>{
-    console.log('test');
+app.get('/getAll', (request, response) =>{
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.getAllData();
+    
+    result
+    .then(data => response.json({data : data}))
+    .catch(err => console.log(err));
+
 });
+
+app.listen(process.env.PORT, () => console.log('app is running'));
